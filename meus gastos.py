e6f4afb66854a876875c7af1adcb3f39ca5af39f
Henrique Lima Cavelagna <limalourdes.208@gmail.com>
2025-10-16 14:31:33 -0300

    Add files via upload

---
+++ meus gastos.py
@@ -0,0 +1,11 @@
+salario = 6000
+aluguel = 500
+água = 120
+luz = 200
+lazer = 1000
+comida = 1000
+
+gastos = salario - aluguel - água - luz - lazer - comida 
+
+
+print(f'oque sobrou foi {salario - gastos}')
